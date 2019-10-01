a53caacd55bfbc28e5d984e4da1bce59324ef9d0
munari2019-2020 <56030081+munari2019-2020@users.noreply.github.com>
2019-10-01 15:46:25 +0200

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,3 @@
+/* FreeCodeCamp challenge 
+"Build a Personal Portfolio Webpage" 
+- https://learn.freecodecamp.org/responsive-web-design/responsive-web-design-projects/build-a-personal-portfolio-webpage */
